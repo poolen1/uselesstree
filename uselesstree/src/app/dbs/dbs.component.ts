343aee6657ba598e861ab34c1a9ba4fcc917d3b2
poolen108 <poolen108@gmail.com>
2020-07-25 17:24:05 -0700

    Initial commit: Homepage

---
+++ uselesstree/src/app/dbs/dbs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dbs',
+  templateUrl: './dbs.component.html',
+  styleUrls: ['./dbs.component.css']
+})
+export class DbsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
